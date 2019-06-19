5cd4ce0d345fd2e397c2cbd177b28ce129caa128
Justin Havel <havelj@udel.edu>
2019-06-19 12:43:36 -0400

    added powerup image that decends the walls

--- DropDownGame/src/gamePackage/View.java
+++ DropDownGame/src/gamePackage/View.java
@@ -17,6 +17,7 @@ public class View extends JPanel {
 	//images
 	private Image background;
 	private Image ballImg;
+	private Image powerupImg;
 	
 	private Ball ball;
 	private LinkedList<Wall> walls;
@@ -39,17 +40,30 @@ public class View extends JPanel {
 		//ball
 		ImageIcon b = new ImageIcon("src/images/Ball.png");
 		ballImg = b.getImage();
+		//powerup
+		ImageIcon p = new ImageIcon("src/images/Power Up.png");
+		powerupImg = p.getImage();
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
+		int count = 0;
+		
 		//background
 		g.drawImage(background, BORDER, BORDER, getWidth(), getHeight(), this);
 		//ball
 		g.drawImage(ballImg, (int)ball.getX(), (int)ball.getY(), null);
-		
+		//powerup
+//		g.drawImage(powerupImg, 250, 250, null);
+		//walls
 		for(int i = 0; i < walls.size(); i++) {
 			g.fillRect((int) walls.get(i).getX(), (int) walls.get(i).getY(), (int) walls.get(i).getWidth(), (int) walls.get(i).getHeight());
+			count++;
+			
+			if (count == 5) {
+				g.drawImage(powerupImg, (int) walls.get(i).getX() + 75, (int) walls.get(i).getY() - 15, null);
+				System.out.println("powerup drawn");
+			}
 		}
 	}
 	
